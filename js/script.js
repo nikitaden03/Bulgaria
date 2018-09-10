8d0492c0ae5b54b090781de15e2d732a953dccc5
Nikita Denisov <dennikita03@gmail.com>
2018-09-10 19:34:39 +0500

    Slider

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.slider').slick({
+        prevArrow: '<div class="comment__arrow comment__arrow_l"><i class="fa fa-angle-left"></i></div>',
+        nextArrow: '<div class="comment__arrow comment__arrow_r"><i class="fa fa-angle-right"></i></div>',
+        dots: true,
+        // dotsClass: 'round__item',
+    });
+});
